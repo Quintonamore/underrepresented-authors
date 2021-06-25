b2ad1516e2a2d5a9e3144c8101fa1ea182afca7d
vickaykikay <83044235+vickaykikay@users.noreply.github.com>
2021-06-25 12:17:16 -0700

    Update about.php

--- websiteFolder/Vicky/about.php
+++ websiteFolder/Vicky/about.php
@@ -1,8 +1,30 @@
+<?php
+session_start();
+require('db.php');
+?>
 <html>
 <head>
 <link rel="stylesheet" href="style.css">
 </head>
 <body>
+<?php
+if(isset($_SESSION['inDB'])&& $_SESSION['inDB']){
+echo "<div class='dropdown'> <button class='dropbtn'>". $_SESSION['user']. "</button> <div class='dropdown-content'>
+    <a href='#'>My Favorites</a>
+    <a href='logout.php'>Logout</a>
+  </div>
+</div> ";
+}
+else{
+    echo "<div class='dropdown'> <button class='dropbtn'>Dropdown</button> <div class='dropdown-content'>
+    <a href='login.php'>log-in</a>
+    <a href='#'>sign up</a>
+    <a href='#'>Link 3</a>
+  </div>
+</div> ";
+
+}
+?>
 <div class = "title">
     About Us
 </div>
