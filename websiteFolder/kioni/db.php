62766c0658f1c2369d254941bf5c4a4ce0665ba4
Quintonamore <teas98@gmail.com>
2021-08-04 23:14:55 -0700

    Dockerfile creation for website
    Using the kioni directory since that seems the most up to date one.
    Additionally renamed the sql export files so that they can be referenced
    easier by docker. Also added support for passing important config
    variables through environment variables rather than hard-coded.

--- websiteFolder/kioni/db.php
+++ websiteFolder/kioni/db.php
@@ -1,18 +1,17 @@
 <!-- connect to database-->
 <?php
 
-$servername = "localhost";
-$username = 'studentweb1';
-$password = 'bipoc1';
+$username = getenv('DB_USER') ?: 'studentweb1';
+$password = getenv('DB_PASSWORD') ?: 'bipoc1';
 $db = 'bipoc_authors';
-$host = 'localhost';
- $port = 8889; 
+$host = getenv('DB_HOST') ?: 'localhost';
+$port = getenv('DB_PORT') ?: 8889;
 $link = mysqli_init();
 $success = mysqli_real_connect(
-   $link, 
-   $host, 
-   $username, 
-   $password, 
+   $link,
+   $host,
+   $username,
+   $password,
    $db,
-    $port
-)OR die('could not connect to MySQL' . mysqli_connect_error($link));
+   $port
+)OR die('could not connect to MySQL' . mysqli_connect_error());
