38923d50ca7bd9f0acd9d179e48706309741f264
kioni-collab <86212205+kioni-collab@users.noreply.github.com>
2021-08-08 12:20:48 -0700

    Add files via upload

--- websiteFolder/kioni/Extention.php
+++ websiteFolder/kioni/Extention.php
@@ -1,6 +1,9 @@
 
 <?php
-
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Credentials: true ");
+header("Access-Control-Allow-Methods: OPTIONS, GET, POST");
+header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control");
 require('db.php');
 // gets what the xhr sent in string format
 $in = file_get_contents('php://input');
