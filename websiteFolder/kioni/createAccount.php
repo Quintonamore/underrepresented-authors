f6782048e274afb5407009e7019dcd17f4ad4f9c
kioni-collab <86212205+kioni-collab@users.noreply.github.com>
2021-08-19 21:35:27 -0700

    Update createAccount.php

--- websiteFolder/kioni/createAccount.php
+++ websiteFolder/kioni/createAccount.php
@@ -60,7 +60,9 @@ if(isset($_POST['create'])){
         </div>"; 
 
     //if not in databse, add to database and send user to log in page
-    $createAccount = "INSERT INTO accounts VALUES('".$userName."', PASSWORD('".$passWord."'), '".$emailAdd."');";
+    //$createAccount = "INSERT INTO accounts VALUES('".$userName."', PASSWORD('".$passWord."'), '".$emailAdd."');";
+    //$sql4 = @mysqli_query($link, $createAccount);
+    $createAccount =  "SELECT * FROM accounts; ";
     $sql4 = @mysqli_query($link, $createAccount);
         echo "after second querry";
          if(!$link){
