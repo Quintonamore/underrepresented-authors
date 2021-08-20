b1eb2a079ca4d81522d45641b4bd99fa3830cf67
kioni-collab <86212205+kioni-collab@users.noreply.github.com>
2021-08-20 00:31:48 -0700

    Add files via upload

--- websiteFolder/kioni/Extention.php
+++ websiteFolder/kioni/Extention.php
@@ -47,18 +47,20 @@ $query .= ";";
 			$_SESSION['description'] = $description;
 			$booklink = $row[11];
 		// what will be the this.responcetext 
-		echo  "<p class='book-theme'> .$ident2 .</p><hr><br>
-                      <img src= ' . $bookcover .  ' alt='cover' width='150' height='230' class='image1'>
-          <p class= 'title1'><b>Title:</b> <a href='  . $booklink . ' target='_blank'>  .$title . </a></p>
-          <p class='author1'><b>By:</b> . $authName.</p>
-          <p class='genre1'><b>Genre:</b> . $genre2.</p>
-          <p class='ISBN1'><b>ISBN-13:</b>  $isbn .</p><br><br><br><br>
+		echo  " <p class='book-theme'>LGBTQ+</p><hr><br>
+                      <img src='https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1604597893l/53802072._SX318_.jpg' alt='cover' width='150' height='230' class='image1'>
+          <p class='title1'><b>Title:</b> <a href='https://www.goodreads.com/book/show/53802072-some-girls-do' target='_blank'>Some Girls Do</a></p>
+          <p class='author1'><b>By:</b> Jennifer Dugan</p>
+          <p class='genre1'><b>Genre:</b> Romance</p>
+          <p class='ISBN1'><b>ISBN-13:</b> 9780593112533</p><br><br><br><br>
 
                       <!--Book Description 1-->
                       <div class='description1'>
                         <details>
                           <summary><b>Book Description</b></summary>
-                            <div class='summary2'> . $description.  </div>
+                            <div class='summary2'>
+Morgan, an elite track athlete, is forced to transfer high schools late in her senior year after it turns out being queer is against her private Catholic schools code of conduct. There, she meets Ruby, who has two hobbies: tinkering with her baby blue 1970 Ford Torino and competing in local beauty pageants, the latter to live out the dreams of her overbearing mother. The two are drawn to each other and cant deny their growing feelings. But while Morgan--out and proud, and determined to have a fresh start--doesnt want to have to keep their budding relationship a secret, Ruby isnt ready to come out yet. With each girl on a different path toward living her truth, can they go the distance together?
+                          </div>
                         </details>
                       </div>
 
