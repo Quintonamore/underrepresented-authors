973d84c4b9ebb434f9eba024bb7e802bc75ae558
nsayre01 <86212194+nsayre01@users.noreply.github.com>
2021-08-28 23:27:06 -0700

    Add files via upload

---
+++ datascraper-goodreads/DelSpecificData.py
@@ -0,0 +1,34 @@
+#This program connects to the database and allows you to view the data set and delete based on manually inputted information before running
+import pyodbc
+
+conn = pyodbc.connect(driver='{MySQL ODBC 8.0 ANSI Driver}',
+                        server='34.82.68.145:3306',
+                        database='bipoc_authors',
+                        uid='root',pwd='underrepresented-authors-5441-hi-mom')
+cursor = conn.cursor()
+print("Here is the current data set")
+cursor.execute('SELECT * FROM bipoc_authors.books_authors')
+for row in cursor:
+    print(row)
+
+print("List of variables: AuthName, BookTitle, Year, Genre, Theme, AuthIdent, Length, ISBN, Approval, bookcover, description, Link")
+var = input("Which do you want to delete by?:")
+data = input("what is the data?:")
+
+sql = "DELETE FROM books_authors WHERE {}='{}'".format(var, data)
+
+try:
+    cursor.execute(sql)
+    cursor.execute('SELECT * FROM bipoc_authors.books_authors')
+
+    for row in cursor:
+        print(row)
+    conn.commit()
+except Exception as e:
+    conn.rollback()
+    print(e)
+
+    cursor.execute('SELECT * FROM bipoc_authors.books_authors')
+
+    for row in cursor:
+        print(row)
--- datascraper-goodreads/main.py
+++ datascraper-goodreads/main.py
@@ -1,14 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
-import pyodbc
+import pyodbc 
 
 print("----------------------------------------------------------")
-
+count = 100
 conn = pyodbc.connect(driver='{MySQL ODBC 8.0 ANSI Driver}',
                         server='34.82.68.145:3306',
                         database='bipoc_authors',
                         uid='root',pwd='underrepresented-authors-5441-hi-mom')
-
+#
+# data = {}
+# data['books'] = []
 sessions_requests = requests.Session()
 login_url = "https://www.goodreads.com/user/sign_in"
 result = sessions_requests.get(login_url)
@@ -42,11 +44,11 @@ headers = {
     'If-None-Match': 'W/^\\^ddae222cb1f6b5cd8483b421666e740e^\\^',
 }
 
-#NB. Original query string below. It seems impossible to parse and
-#reproduce query strings 100% accurately so the one below is given
-#in case the reproduced version is not "correct".
-# response = requests.get('https://www.goodreads.com/shelf/show/bipoc?page=3', headers=headers, cookies=cookies)
-
+# #NB. Original query string below. It seems impossible to parse and
+# #reproduce query strings 100% accurately so the one below is given
+# #in case the reproduced version is not "correct".
+# # response = requests.get('https://www.goodreads.com/shelf/show/bipoc?page=3', headers=headers, cookies=cookies)
+#
 for page in range(1,2):
   print(str(page))
   # BIPOC LINK: https://www.goodreads.com/shelf/show/bipoc?page=
@@ -66,6 +68,7 @@ for page in range(1,2):
   books = all_books.find_all(class_="elementList")
 
   for x in books:
+    count += 1
     # Extracts title and author
     title = x.find(class_="bookTitle").get_text()
     author = x.find(class_="authorName").get_text()
@@ -77,27 +80,27 @@ for page in range(1,2):
 
     # title = newSoup.find(id ="bookTitle").get_text().strip()
     # author = newSoup.find(class_="authorName").get_text()
-    # print(info_link)
+
     # print(title)
     # print(author)
 
     # Extracts number of pages
     numPages = newSoup.find(itemprop ="numberOfPages").get_text().split(" ")
     numPages = numPages[0]
-    # print(numPages)
+    print(numPages)
     if int(numPages) > 200:
       length = "novel"
-    elif int(numPages) > 5:
-      length = "short story"
-    else:
+    elif int(numPages) < 5:
       length = "poem"
+
 
     # Extracts publishing date
     pubDate = newSoup.find(id ="details").get_text().split("\n")
     pubDate = pubDate[4].strip()
     pubYear = pubDate.split(" ")
     pubYear = pubYear[2]
-    # print(pubYear)
+    # print(pubDate)
+
 
     # Extracts ISBN
     isbn = newSoup.find_all(class_="infoBoxRowItem")
@@ -106,7 +109,7 @@ for page in range(1,2):
     # Check if ISBN is a number
     if not isbn.isdecimal():
       isbn = "Unknown"
-    # print(isbn)
+    print(isbn)
 
     # Extracts ISBN13
     isbn13 = newSoup.find_all(class_="infoBoxRowItem")
@@ -115,8 +118,8 @@ for page in range(1,2):
     isbn13 = isbn13.replace(")", "")
     # Check if ISBN is a number
     if not isbn13.isdecimal():
-      isbn13 = 12345
-    print(isbn13)
+      isbn13 = 12345 + count
+    # print(isbn13)
 
     # Extracts book description
     description = newSoup.find_all(id = "description")
@@ -131,26 +134,50 @@ for page in range(1,2):
     genre_list = newSoup.find_all(class_="actionLinkLite bookPageGenreLink")
     for genre in genre_list:
       genres.append(genre.text)
-    divider = ", "
-    genres = divider.join(genres)
-    # print(genres)
 
+    genres = ", ".join(genres)
+    # print(genres)
     # Extracts cover image
 
     cover_container = newSoup.find(class_ = "bookCoverPrimary")
     cover = cover_container.find('img')
     cover_link = cover.attrs['src']
-    # print(cover_link)
-    # print("\n")
-    full_link = 'https://www.goodreads.com' + info_link
-    # Creates data variable
-    print("----------------------------------------------------------")
+    print(cover_link)
+    print("\n")
+
+
+
+
+    # Appends book data to data list
+    # data['books'].append(
+    #   {
+    #     'title': title,
+    #     'author': author,
+    #     'numPages': numPages,
+    #     'genres': genres,
+    #     'isbn': isbn,
+    #     'isbn13': isbn13,
+    #     'description': description,
+    #     'pubDate': pubDate,
+    #     'cover_link': cover_link
+    #   }
+    # )
+
     # data = author + ", " + title + ", " + pubDate + ", " + genres + ", " + theme + ", " + authID + ", " + length + ", " + isbn13 + ", " + "0.0, " + cover_link + ", " + description + ", " + info_link
-    sql = """INSERT INTO books_authors(AuthName, BookTitle, Year, Genre, Theme, AuthIdent, Length, ISBN, Approval, bookcover, description, Link) VALUES('{}', '{}', {}, '{}', '{}', '{}', '{}', {}, 0.0, '{}', '{}', '{}')""".format(author, title, pubYear, genres, theme, authID, length, isbn13, cover_link, description, full_link)
+
+# print("----------------------------------------------------------")
+#
+#
+# print("----------------------------------------------------------")
+#
+#
+# print("----------------------------------------------------------")
+# sql = """INSERT INTO books_authors(AuthName, BookTitle, Year, Genre, Theme, AuthIdent, Length, ISBN, Approval, bookcover, description, Link) VALUES('Michelle Obama', 'Becoming (Hardcover)', 2018, 'Nonfiction, Autobiography, Memoir, Biography, Audiobook, Biography, Autobiography, Politics, Biography Memoir, Feminism, Adult, History', 'BIPOC', 'N/A', 'novel', 12345, 0.0, 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1528206996l/38746485.jpg', 'In a life filled with meaning and accomplishment, Michelle Obama has emerged as one of the most iconic and compelling women of our era. As First Lady of the United States of America—the first African American to serve in that role—she helped create the most welcoming and inclusive White House in history, while also establishing herself as a powerful advocate for women and girls in the U.S. and around the world, dramatically changing the ways that families pursue healthier and more active lives, and standing with her husband as he led America through some of its most harrowing moments. Along the way, she showed us a few dance moves, crushed Carpool Karaoke, and raised two down-to-earth daughters under an unforgiving media glare.  In her memoir, a work of deep reflection and mesmerizing storytelling, Michelle Obama invites readers into her world, chronicling the experiences that have shaped her—from her childhood on the South Side of Chicago to her years as an executive balancing the demands of motherhood and work, to her time spent at the world’s most famous address. With unerring honesty and lively wit, she describes her triumphs and her disappointments, both public and private, telling her full story as she has lived it—in her own words and on her own terms. Warm, wise, and revelatory, Becoming is the deeply personal reckoning of a woman of soul and substance who has steadily defied expectations—and whose story inspires us to do the same.', '/book/show/38746485-becoming')"""
+# sql = """INSERT INTO books_authors(AuthName, BookTitle, Year, Genre, Theme, AuthIdent, Length, ISBN, Approval, bookcover, description, Link) VALUES('Michelle Obama', 'Becoming (Hardcover)', 2018, 'Nonfiction, Autobiography, Memoir, Biography, Audiobook, Biography, Autobiography, Politics, Biography Memoir, Feminism, Adult, History', 'BIPOC', 'N/A', 'novel', 12345, 0.0,'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1528206996l/38746485.jpg', 'Test Description', 'test link')"""
+    sql = """INSERT INTO books_authors(AuthName, BookTitle, Year, Genre, Theme, AuthIdent, Length, ISBN, Approval, bookcover, description, Link) VALUES('{}', '{}', {}, '{}', '{}', '{}', '{}', {}, 0.0,'{}', '{}', '{}')""".format(author, title, pubYear, genres, theme, authID, length, isbn13, cover_link, description, info_link)
+    # sql = "DELETE FROM books_authors WHERE AuthName ='Author Name'"
     print(sql)
-    print("----------------------------------------------------------")
     cursor = conn.cursor()
-    print("----------------------------------------------------------")
     try:
         cursor.execute(sql)
         cursor.execute('SELECT * FROM bipoc_authors.books_authors')
@@ -158,15 +185,16 @@ for page in range(1,2):
         for row in cursor:
             print(row)
         conn.commit()
-    except:
+    except Exception as e:
         conn.rollback()
-        print("Didn't work")
-    print("----------------------------------------------------------")
+        print(e)
 
+        cursor.execute('SELECT * FROM bipoc_authors.books_authors')
 
+        for row in cursor:
+            print(row)
 
-
-
-
-# sql = """INSERT INTO books_authors(AuthName, BookTitle, Year, Genre, Theme, AuthIdent, Length, ISBN, Approval, bookcover, description, Link) VALUES(data)"""
+# Print data dictionary to data text file
+# with open('data.txt', 'w') as f:
+#   print(data, file=f)
 
