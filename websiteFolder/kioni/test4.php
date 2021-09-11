c5cfdf68045dc557b966ad079f84d96d76f03632
kioni-collab <86212205+kioni-collab@users.noreply.github.com>
2021-09-11 13:08:50 -0700

    Add files via upload

--- websiteFolder/kioni/test4.php
+++ websiteFolder/kioni/test4.php
@@ -2,8 +2,7 @@
 session_start();
 require('db.php');
 
-$query = "
-ALTER TABLE `books_authors` ADD PRIMARY KEY (`Bookid`); ALTER TABLE `books_authors` MODIFY `Bookid` double NOT NULL AUTO_INCREMENT;
+$query = " ALTER TABLE `books_authors` ADD `Bookid` DOUBLE NOT NULL AFTER `Link`; ALTER TABLE `books_authors` ADD PRIMARY KEY (`Bookid`); ALTER TABLE `books_authors` MODIFY `Bookid` double NOT NULL AUTO_INCREMENT;
  ";
  
  $sql = @mysqli_multi_query($link, $query);
